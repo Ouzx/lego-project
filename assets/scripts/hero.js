270eb639f8971e6298234280ec6f0e2182e8909e
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-16 13:06:39 +0300

    slider is working, but not responsible

--- assets/scripts/hero.js
+++ assets/scripts/hero.js
@@ -1,4 +1,4 @@
-const slides = [
+const slidesData = [
   {
     text: "What's on sale now?",
     title: "Shop now",
@@ -19,49 +19,43 @@ const slides = [
     title: "*Learn more",
     link: "#",
   },
-  {
-    text: "FREE delivery on orders above £50!",
-    title: "*Learn more",
-    link: "#",
-  },
 ];
 
 const headerTopBar = document.querySelector(".header-top");
 const sliderContainer = headerTopBar.querySelector(".content");
 
 let currentSlide = 0;
-const createSlide = (slide, index) => {
+const createSlide = (slide) => {
   return `
-        <div class="slide" ${index !== currentSlide && "aria-hidden=true"}>
+        <div class="slide">
         <span class="slide__text">${slide.text}</span>
         <a href="${slide.link}" class="slide__link">${slide.title}</a>
         </div>
     `;
 };
 
-const slidesContent = slides.map(createSlide).join("");
+const slidesContent = slidesData.map(createSlide).join("");
 sliderContainer.innerHTML = slidesContent;
 
+const slides = sliderContainer.querySelectorAll(".slide");
 const nextButton = headerTopBar.querySelector("#next-slide");
 const prevButton = headerTopBar.querySelector("#prev-slide");
 
+const width = slides[0].offsetWidth;
+
+// change sliders position to start from the first slide
+sliderContainer.style.left = `${width * 1.5}px`;
+
 const onNextSlide = () => {
   const nextSlide = currentSlide + 1;
-  currentSlide = nextSlide === slides.length ? 0 : nextSlide;
-  updateSlides();
+  currentSlide = nextSlide >= slides.length ? 0 : nextSlide;
+  sliderContainer.style.transform = `translateX(-${width * currentSlide}px)`;
 };
 
 const onPrevSlide = () => {
-  const prevSlide = currentSlide - 1;
-  currentSlide = prevSlide < 0 ? slides.length - 1 : prevSlide;
-  updateSlides();
-};
-
-const updateSlides = () => {
-  const slides = sliderContainer.querySelectorAll(".slide");
-  slides.forEach((slide, index) => {
-    slide.setAttribute("aria-hidden", index !== currentSlide);
-  });
+  const nextSlide = currentSlide - 1;
+  currentSlide = nextSlide < 0 ? slides.length - 1 : nextSlide;
+  sliderContainer.style.transform = `translateX(-${width * currentSlide}px)`;
 };
 
 nextButton.addEventListener("click", onNextSlide);
