ba43a73a9c1504848d627ac5d2fe58ceeb292455
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-17 20:00:33 +0300

    smart header added

--- assets/scripts/hero.js
+++ assets/scripts/hero.js
@@ -5,6 +5,7 @@ const closeTopBar = () => {
   topBar.style.display = "none";
 };
 
+// Menu Toggle
 const cross = document.querySelector(".cross");
 cross.addEventListener("click", closeTopBar);
 
@@ -17,3 +18,40 @@ const toggleMenu = () => {
   }
   const menu = document.querySelector(".header-menu");
 };
+
+// Sticky Header
+const nav = document.querySelector("header");
+const supportPageOffset = window.pageXOffset !== undefined;
+const isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  // let scrolledPosition = supportPageOffset
+  //   ? window.pageYOffset
+  //   : isCSS1Compat
+  //   ? document.documentElement.scrollTop
+  //   : document.body.scrollTop;
+  let scrolledPosition = window.pageYOffset;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+};
+
+const handleNavScroll = () => {
+  if (isScrollingDown() && !nav.contains(document.activeElement)) {
+    nav.classList.add("scroll-down");
+    nav.classList.remove("scroll-up");
+  } else {
+    nav.classList.add("scroll-up");
+    nav.classList.remove("scroll-down");
+  }
+};
+
+window.addEventListener("scroll", handleNavScroll);
