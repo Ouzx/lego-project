f78c76f397d744003b0ecc048eff8ca655b5d4e7
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-26 18:10:57 +0300

    side bar for 1200px

--- assets/scripts/hero/desktopSidebar.js
+++ assets/scripts/hero/desktopSidebar.js
@@ -1,3 +1,5 @@
+const desktopMenuWrapper = document.querySelector(".desktop-menu-wrapper");
+const desktopMenuSeperator = document.querySelector(".desktop-menu-seperator");
 // menu and sub menu 1
 const desktopMenuContainer = document.querySelector(".desktop-menu-container");
 const desktopMenuItems = document.querySelector(".desktop-menu-items");
@@ -6,6 +8,9 @@ let desktopMenuState = false;
 // sub menu 2
 const desktopSubMenuTitle = document.querySelector(
   ".desktop-inner-menu-items > p"
+);
+const desktopSubMenuContainer = document.querySelector(
+  ".desktop-inner-menu-items-container"
 );
 
 const closeDesktopMenu = () => {
@@ -17,7 +22,9 @@ const closeDesktopMenu = () => {
 };
 
 //desktop sub menu 1
+let lastMenu = null;
 const openDesktopMenu = async (menuName) => {
+  if (lastMenu != menuName) closeDesktopSubMenu();
   handleDesktopMenuResize();
 
   menuButtonStateHandler(menuName);
@@ -33,12 +40,12 @@ const openDesktopMenu = async (menuName) => {
   for (item of Object.entries(menuData)) {
     if (item[1].length === 0)
       menuItems.push(/*html*/ `
-        <button class="desktop-menu-item" > ${item[0]} </button>
+        <button class="desktop-menu-item" onclick="clearSubMenuFocus()" ><span>${item[0]}</span>  </button>
         `);
     else {
       menuItems.push(/*html*/ `
-        <button class="desktop-menu-item" onclick="openSubMenu2('${item[0]}', '${menuName}')" > 
-            ${item[0]} 
+        <button class="desktop-menu-item" onclick="openDesktopSubMenu('${item[0]}', '${menuName}', this)" > 
+            <span>${item[0]}</span> 
             <img src="assets/icons/hero/next.svg" width="8" />
         </button>
         `);
@@ -48,19 +55,80 @@ const openDesktopMenu = async (menuName) => {
   desktopMenuItems.innerHTML = /*html*/ `${menuItems.join("")}`;
 
   desktopMenuState = true;
+  lastMenu = menuName;
+};
+
+// desktop sub menu 2
+let subMenuRef = null;
+const openDesktopSubMenu = async (menuName, parentName, button) => {
+  if (!button) return;
+  if (button === subMenuRef) {
+    clearSubMenuFocus();
+    return;
+  } else {
+    if (subMenuRef) {
+      subMenuRef.classList.remove("desktop-menu-item-active");
+    }
+  }
+
+  subMenuRef = button;
+  subMenuRef.classList.add("desktop-menu-item-active");
+  desktopMenuSeperator.style.display = "block";
+
+  const data = await getSiderBarData();
+
+  const menuData = data[parentName][menuName];
+
+  let menuItems = [];
+
+  menuData.map((item, i) => {
+    if (item === menuData[menuData.length - 1] && parentName === "shop")
+      desktopSubMenuTitle.innerHTML = item;
+    else
+      menuItems.push(/*html*/ `
+      <button class="desktop-sub-menu-item"> ${item} </button>
+    `);
+  });
+  if (parentName !== "shop") desktopSubMenuTitle.innerHTML = "";
+
+  desktopSubMenuContainer.innerHTML = `${menuItems.join("")}`;
+
+  desktopMenuWrapper.ariaExpanded = true;
+};
+
+const closeDesktopSubMenu = () => {
+  desktopSubMenuContainer.innerHTML = "";
+  desktopSubMenuTitle.innerHTML = "";
+  desktopMenuWrapper.ariaExpanded = false;
+  desktopMenuSeperator.style.display = "none";
+};
+
+const clearSubMenuFocus = () => {
+  closeDesktopSubMenu();
+  if (!subMenuRef) return;
+  if (!subMenuRef.classList.contains("desktop-menu-item-active")) return;
+  subMenuRef.classList.remove("desktop-menu-item-active");
+  subMenuRef = null;
 };
 
 // extra left padding calculation for desktop menu
 const handleDesktopMenuResize = () => {
-  const startPadding = 8.5 * 16;
+  const startPadding = 8.5;
   const width = window.innerWidth;
+  if (width < 1600) {
+    desktopMenuContainer.style.paddingLeft = startPadding + "rem";
+    return;
+  }
   const diff = width - 1600;
+  const diffAsRem = diff / 32; // 16 comes from rem, 2 comes from formula
+  // diff / 2 + startPadding + "px"
   if (diff > 0)
-    desktopMenuContainer.style.paddingLeft = diff / 2 + startPadding + "px";
+    desktopMenuContainer.style.paddingLeft = diffAsRem + startPadding + "rem";
 };
 
 window.addEventListener("resize", handleDesktopMenuResize);
 
+// focusing on the menu item
 const desktopMenuShopButton = document.querySelector("#shop");
 const desktopMenuDiscoverButton = document.querySelector("#discover");
 const desktopMenuHelpButton = document.querySelector("#help");
