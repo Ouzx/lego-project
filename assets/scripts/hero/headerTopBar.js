60964dbf0e51ee2a39c862de2c865f08c076f2ce
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-06 00:43:29 +0300

    Ozkan part first commit

--- assets/scripts/hero/headerTopBar.js
+++
@@ -1,8 +0,0 @@
-const closeTopBar = () => {
-  const topBar = document.querySelector(".header-top");
-  console.log(topBar);
-  topBar.style.display = "none";
-};
-
-const cross = document.querySelector(".cross");
-cross.addEventListener("click", closeTopBar);
